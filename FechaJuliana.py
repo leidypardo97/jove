be315ce42b4752653771d7536ae890119e62d8e6
Julián <julianorla7@hotmail.com>
2014-06-14 15:23:06 -0500

    Update FechaJuliana.py
    Ahora se puede elegir lo que se desea calcular entre la fecha normal y la fecha juliana. Próximamente podrá decir qué día de la semana es en los dos casos.

--- FechaJuliana.py
+++ FechaJuliana.py
@@ -1,13 +1,28 @@
 import sys
 k=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-y=int(raw_input("insert the year with all the digits > "))
-m=int(raw_input("insert month > "))
-if m>12:
-    print "A year only has 12 months"
-    sys.exit()
-d=int(raw_input("insert day > "))
+mean=int(raw_input("From Normal Date to Julian Date type 0, from Julian Date to Normal Date write 1 > "))
 
-J=367*y-((7*(y+(m+9)/12))/4)-((3*((y+(m-9)/7)/100+1))/4)+275*m/9+d+1721029
+if mean==0:
+    y=int(raw_input("insert the year with all the digits > "))
+    m=int(raw_input("insert month > "))
+    if m>12:
+        print "A year only has 12 months"
+        sys.exit()
+    d=int(raw_input("insert day > "))
+    J=367*y-((7*(y+(m+9)/12))/4)-((3*((y+(m-9)/7)/100+1))/4)+275*m/9+d+1721029
+    print "The Julian Date for the noon of", k[m-1], d,",", y, "is ",J
 
-print "The Julian Date for the noon of", k[m-1], d,",", y, "is ",J
+if mean==1:
+    Q=int(raw_input("Write your Julian Date Here > "))
+    z=Q+68569
+    a=4*z/146097
+    s=z-((146097*a)+3)/4
+    u=(4000*(s+1))/1461001
+    r=s-(1461*u)/4+31
+    f=80*r/2447
+    d=r-(2447*f)/80
+    g=f/11
+    m=f+2-(12*g)
+    y=100*(a-49)+u+g
+    print "The Corresponding Date for", Q," is:", k[m-1], d,", ", y
